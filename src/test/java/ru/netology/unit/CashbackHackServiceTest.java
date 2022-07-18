a854eafce306f85ed652b74ff183c2366f5e145e
boarderbare <boarderbare@gmail.com>
2022-07-18 13:18:11 +0400

    join on platformJunit with use junit4 and junitjupiter

--- src/test/java/ru/netology/unit/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/unit/CashbackHackServiceTest.java
@@ -1,8 +1,10 @@
 package ru.netology.unit;
 
+import org.junit.jupiter.api.Test;
 import ru.netology.unit.CashbackHackService;
 
 import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CashbackHackServiceTest {
 
@@ -16,6 +18,22 @@ public class CashbackHackServiceTest {
     }
     @org.junit.Test
     public void shouldGetRemainMore() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 3000;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void shouldGetRemainJ5() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount=1800;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 200;
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void shouldGetRemainMoreJ5() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 3000;
         int actual = cashbackHackService.remain(amount);

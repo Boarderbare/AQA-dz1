e2b703e47309032777c7f12308083853d7b188e5
boarderbare <boarderbare@gmail.com>
2022-07-18 12:00:16 +0400

    change Junit5 to TestNJ

--- src/test/java/ru/netology/unit/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/unit/CashbackHackServiceTest.java
@@ -1,26 +1,25 @@
 package ru.netology.unit;
 
-import org.junit.jupiter.api.Test;
 import ru.netology.unit.CashbackHackService;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.testng.Assert.*;
 
 public class CashbackHackServiceTest {
 
-    @Test
+    @org.testng.annotations.Test
     public void shouldGetRemain() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount=1800;
         int actual = cashbackHackService.remain(amount);
         int expected = 200;
-        assertEquals(expected, actual);
+        assertEquals(actual,expected);
     }
-    @Test
+    @org.testng.annotations.Test
     public void shouldGetRemainMore() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 3000;
         int actual = cashbackHackService.remain(amount);
         int expected = 0;
-        assertEquals(expected, actual);
+        assertEquals(actual,expected);
     }
 }
